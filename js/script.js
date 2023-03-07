10bc94e873c4969013eebc2fe3d93a6bf5eddfe6
CodingClassesTNO <126271635+CodingClassesTNO@users.noreply.github.com>
2023-03-07 13:57:32 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const element = document.getElementById("currentYear");
+
+const currentYr = new Date();
+element.innerText = currentYr.getFullYear();
+// element.innerText = currentYr.getSeconds();
+
+
+
+
+
+
+// element.innerHTML="Hello";
+
+// document.getElementById("currentYear").innerHTML="Hello";
